f9956e63192a10330fbdd105ff2cf4405912e535
wei.lei <wei.lei@infaith.com.cn>
2022-05-16 21:48:01 +0800

    使用Lambda来简化代码

--- iris/iris-client/src/test/java/com/leiwei2094/iris/client/ClientTest.java
+++ iris/iris-client/src/test/java/com/leiwei2094/iris/client/ClientTest.java
@@ -17,31 +17,27 @@ public class ClientTest {
         IRegistry registry = new EtcdRegistry("http://127.0.0.1:2379");
         RpcClient client = new RpcClient(registry);
 
-        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    IHelloService helloService = client.create(IHelloService.class);
-                    String s = helloService.hello("leo");
-                    System.out.println("====" + s);
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+            try {
+                IHelloService helloService = client.create(IHelloService.class);
+                String s = helloService.hello("leo");
+                System.out.println("====" + s);
 
-                    String s2 = helloService.hello("tom");
-                    System.out.println("====" + s2);
+                String s2 = helloService.hello("tom");
+                System.out.println("====" + s2);
 
-                    String s3 = helloService.hello("jerry");
-                    System.out.println("====" + s3);
+                String s3 = helloService.hello("jerry");
+                System.out.println("====" + s3);
 
-                    System.out.println("==== rpc invoke finished...");
-                    Thread.sleep(2 * 1000);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
+                System.out.println("==== rpc invoke finished...");
+                Thread.sleep(2 * 1000);
+            }catch (Exception e){
+                e.printStackTrace();
             }
         },5,3, TimeUnit.SECONDS);
 
         Thread.sleep(3000 * 1000);
     }
-
 
     @Test
     public void test2() throws Exception {
@@ -49,7 +45,7 @@ public class ClientTest {
         RpcClient client = new RpcClient(registry);
 
         com.leiwei2094.iris.core.IHelloService helloService = client.create(com.leiwei2094.iris.core.IHelloService.class);
-        String s = helloService.hello("haha");
+        String s = helloService.hello("Hello");
         System.out.println(s);
     }
 }
